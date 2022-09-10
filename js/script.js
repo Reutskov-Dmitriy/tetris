2b58f82a51174f1ff5af6d5bad743907a9640b81
Reutskov-Dmitriy <infowelc@gmail.com>
2022-09-10 13:12:30 +0300

    add doesCollide

--- js/script.js
+++ js/script.js
@@ -30,8 +30,10 @@ const brick = {
 		[0, 0, 0],
 	],
 	y: 7,
-	x: 3,
+	x: 7,
 }
+
+// tick()
 
 render()
 
@@ -43,5 +45,25 @@ function render() {
 	brick.state.forEach((row, y) => row.forEach((i, x) => {
 		if (i) board.rows[y + brick.y].cells[x + brick.x].className = colors[i]
 	}))
+}
+
+function tick() {
+	if (doesCollide()) {
+
+	} else {
+		brick.y += 1;
+	}
+	render()
+
+	setTimeout(tick, 1000)
+}
+
+function doesCollide() {
+	return brick.state.some((row, y) => row.some((i, x) => {
+		return i && state[y + brick.y]?.[x + brick.x]
+	})) || brick.state.some((row, y) => row.some((i, x) => {
+		return i &&
+			(y + brick.y < 0 || y + brick.y > 19 || x + brick.x < 0 || x + brick.x > 9)
+	}));
 }
 
